636a973bc7774c19e3c03a0f72e874f750b13329
Matt Dean <mdean400@gmail.com>
2023-03-11 10:26:43 -0700

    Fix auth sign in twice bug and move everything to src directory

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 // @ts-check
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { env } = require("./server/env");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    runtime: "experimental-edge",
     appDir: true,
+    typedRoutes: true,
   },
   typescript: {
     // TODO: turn this off once we get things more stable
