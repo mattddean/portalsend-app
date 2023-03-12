ec68b5295f1995a7f4de18a7d4cf8a886d0d955e
Matt Dean <mdean400@gmail.com>
2023-03-11 22:32:02 -0700

    Update test route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,14 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { pathname } = new URL(request.url);
+
+  const theCookies = cookies();
+
+  theCookies.getAll().forEach((cookie) => {
+    console.debug(cookie.name, ": ", cookie.value);
+  });
 
   console.debug("pathname", pathname);
 
