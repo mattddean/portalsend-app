229c8df24f21723787e3bbb1e58cb24e56bb7b6d
Matt Dean <mdean400@gmail.com>
2023-03-11 22:44:15 -0700

    More edge updates

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,14 +1,16 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: Request) {
   const { pathname } = new URL(request.url);
 
   const theCookies = cookies();
 
-  theCookies.getAll().forEach((cookie) => {
-    console.debug(cookie.name, ": ", cookie.value);
-  });
+  console.debug("here are the cookies");
+  const jar = theCookies.getAll().map((cookie) => `${cookie.name}=${cookie.value}`);
+  console.debug("jar", jar.join(","));
 
   console.debug("pathname", pathname);
 
